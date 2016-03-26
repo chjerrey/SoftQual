b9bfa1e1f6baafa672b6e3dee9ac322408799451
Monika Drößler <monikadroessler@aol.de>
2016-03-26 23:25:20 +0100

    Documentation extanded - not finished

--- tests/conf.js
+++ tests/conf.js
@@ -13,7 +13,11 @@ exports.config = {
     
     suites: { // die Testfälle
         login: ['spec/login/login_no_success.js', 'spec/login/login_no_mail.js', 'spec/login/login_invalid_mail.js', 'spec/login/login_no_password.js', 'spec/login/login_success.js'],
-        logout: ['spec/logout/logout_logout_new_device.js', 'spec/logout/logout_success.js']
+        device: ['spec/device/device_delete_no_success.js'],
+        profile: ['spec/profile/profile_delete.js'],
+        logout: ['spec/logout/logout_logout_new_device.js', 'spec/logout/logout_success.js'],
+        forgot_password: ['spec/forgot_password/forgot_password_no_mail.js'],
+        register: ['spec/register/register_link_login.js']
     },
 
     defaultMail: 'admin@ceventis.com', // Standardwert Mail
